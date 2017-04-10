f07ddaee75613375a2f657e9b78402a93692cd4c
Kodey Converse <kodey@krconv.com>
2017-04-10 14:11:18 -0400

    WIP to get DNS packet rewriting working.

--- src/main/java/cs4516/team4/AccessController.java
+++ src/main/java/cs4516/team4/AccessController.java
@@ -1,42 +1,69 @@
 /**
- * 
+ * A DNS-based Capabilities Access Controller.
  */
 package cs4516.team4;
 
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
+import org.projectfloodlight.openflow.protocol.OFFactory;
+import org.projectfloodlight.openflow.protocol.OFFlowAdd;
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFType;
+import org.projectfloodlight.openflow.protocol.action.OFAction;
+import org.projectfloodlight.openflow.protocol.match.MatchField;
+import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.IPv4Address;
+import org.projectfloodlight.openflow.types.IpProtocol;
+import org.projectfloodlight.openflow.types.MacAddress;
+import org.projectfloodlight.openflow.types.OFBufferId;
+import org.projectfloodlight.openflow.types.OFPort;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import cs4516.team4.dns.DNS;
+import cs4516.team4.dns.DNSResource;
 import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IFloodlightProviderService;
 import net.floodlightcontroller.core.IOFMessageListener;
 import net.floodlightcontroller.core.IOFSwitch;
 import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.EthernetTest;
+import net.floodlightcontroller.packet.IPv4;
+import net.floodlightcontroller.packet.UDP;
 
 /**
  * @author Team 4
- *
  */
 public class AccessController implements IOFMessageListener, IFloodlightModule {
 
 	protected IFloodlightProviderService floodlightProvider;
-	protected Set<Long> macAddresses;
+	private MacAddress dnsMacAddress;
+	private MacAddress webserverMacAddress;
 	protected static Logger logger;
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see net.floodlightcontroller.core.IListener#getName()
 	 */
 	@Override
 	public String getName() {
-		return MACTracker.class.getSimpleName();
+		return AccessController.class.getSimpleName();
 	}
 
-	/* (non-Javadoc)
-	 * @see net.floodlightcontroller.core.IListener#isCallbackOrderingPrereq(java.lang.Object, java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.floodlightcontroller.core.IListener#isCallbackOrderingPrereq(java.
+	 * lang.Object, java.lang.String)
 	 */
 	@Override
 	public boolean isCallbackOrderingPrereq(OFType type, String name) {
@@ -44,8 +71,12 @@ public class AccessController implements IOFMessageListener, IFloodlightModule {
 		return false;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.floodlightcontroller.core.IListener#isCallbackOrderingPostreq(java.lang.Object, java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.floodlightcontroller.core.IListener#isCallbackOrderingPostreq(java.
+	 * lang.Object, java.lang.String)
 	 */
 	@Override
 	public boolean isCallbackOrderingPostreq(OFType type, String name) {
@@ -53,8 +84,12 @@ public class AccessController implements IOFMessageListener, IFloodlightModule {
 		return false;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.floodlightcontroller.core.module.IFloodlightModule#getModuleServices()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.floodlightcontroller.core.module.IFloodlightModule#getModuleServices(
+	 * )
 	 */
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
@@ -62,8 +97,11 @@ public class AccessController implements IOFMessageListener, IFloodlightModule {
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.floodlightcontroller.core.module.IFloodlightModule#getServiceImpls()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.floodlightcontroller.core.module.IFloodlightModule#getServiceImpls()
 	 */
 	@Override
 	public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
@@ -71,148 +109,150 @@ public class AccessController implements IOFMessageListener, IFloodlightModule {
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.floodlightcontroller.core.module.IFloodlightModule#getModuleDependencies()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.floodlightcontroller.core.module.IFloodlightModule#
+	 * getModuleDependencies()
 	 */
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
-		Collection<Class<? extends IFloodlightService>> l =
-        	new ArrayList<Class<? extends IFloodlightService>>();
-	    l.add(IFloodlightProviderService.class);
-	    return l;
+		Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
+		l.add(IFloodlightProviderService.class);
+		return l;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.floodlightcontroller.core.module.IFloodlightModule#init(net.floodlightcontroller.core.module.FloodlightModuleContext)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.floodlightcontroller.core.module.IFloodlightModule#init(net.
+	 * floodlightcontroller.core.module.FloodlightModuleContext)
 	 */
 	@Override
 	public void init(FloodlightModuleContext context) throws FloodlightModuleException {
 		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
-	    macAddresses = new ConcurrentSkipListSet<Long>();
-	    logger = LoggerFactory.getLogger(MACTracker.class);
+		logger = LoggerFactory.getLogger(AccessController.class);
 
+		Map<String, String> configParameters = context.getConfigParams(this);
+		if (!configParameters.containsKey("dnsMacAddress"))
+			throw new FloodlightModuleException("\"dnsMacAddress\" not set in configuration!");
+		else if (!configParameters.containsKey("webserverMacAddress"))
+			throw new FloodlightModuleException("\"webserverMacAddress\" not set in configuration!");
+
+		dnsMacAddress = MacAddress.of(configParameters.get("dnsMacAddress"));
+		webserverMacAddress = MacAddress.of(configParameters.get("webserverMacAddress"));
 	}
 
-	/* (non-Javadoc)
-	 * @see net.floodlightcontroller.core.module.IFloodlightModule#startUp(net.floodlightcontroller.core.module.FloodlightModuleContext)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.floodlightcontroller.core.module.IFloodlightModule#startUp(net.
+	 * floodlightcontroller.core.module.FloodlightModuleContext)
 	 */
 	@Override
 	public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {
 		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
-
 	}
 
-
-
-	/* (non-Javadoc)
-	 * @see net.floodlightcontroller.core.IOFMessageListener#receive(net.floodlightcontroller.core.IOFSwitch, org.projectfloodlight.openflow.protocol.OFMessage, net.floodlightcontroller.core.FloodlightContext)
+	/**
+	 * Handles a PACKET_IN message.
+	 * 
+	 * @param sw
+	 *            The switch that sent the message.
+	 * @param msg
+	 *            The message to handle.
+	 * @param cntx
+	 *            The Floodlight context.
+	 * 
+	 * @return A command determining whether the message should be processed
+	 *         further.
 	 */
 	@Override
-	public net.floodlightcontroller.core.IListener.Command receive(IOFSwitch sw, OFMessage msg,
-			FloodlightContext cntx) {
+	public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+		Ethernet ethernet = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+		MacAddress sourceMac = ethernet.getSourceMACAddress();
+		MacAddress destMac = ethernet.getDestinationMACAddress();
 
-		switch (msg.getType()) {
-			case PACKET_IN:
-				Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
-		 
-				MacAddress srcMac = eth.getSourceMACAddress();
-				VlanVid vlanId = VlanVid.ofVlan(eth.getVlanID());
-		 
-				if (eth.getEtherType() == EthType.IPv4) {
-					IPv4 ipv4 = (IPv4) eth.getPayload();
-					 
-					byte[] ipOptions = ipv4.getOptions();
-					IPv4Address dstIp = ipv4.getDestinationAddress();
-					 
-					if (ipv4.getProtocol() == IpProtocol.TCP) {
-						TCP tcp = (TCP) ipv4.getPayload();
-		  
-						TransportPort srcPort = tcp.getSourcePort();
-						TransportPort dstPort = tcp.getDestinationPort();
-						short flags = tcp.getFlags();
-						 
+		if (ethernet.getEtherType() != EthType.IPv4)
+			return Command.CONTINUE;
 
-						if (CapabilitiesManager.getInstance().verifyRecord(dstIp) == CapabilitiesManager.Action.ALLOW) {
-							// TODO: ALLOW PACKET
-							System.out.println("Allowing packet to flow");
-						} else {
-							// TODO: DROP PACKET
-							System.out.println("Dropping packet!");
-						}
-						
-					} else if (ipv4.getProtocol() == IpProtocol.UDP) {
-						UDP udp = (UDP) ipv4.getPayload();
-		  
-						TransportPort srcPort = udp.getSourcePort();
-						TransportPort dstPort = udp.getDestinationPort();
-						
-						
-						long ttl = extractTTLfromDNS(udp);
+		logger.debug("Recieved PACKET_IN from switch on " + ((InetSocketAddress) sw.getInetAddress()).getHostName());
 
-						byte[] newIP = CapabilitiesManager.getInstance().addRecord(ttl);
+		IPv4 ip = (IPv4) ethernet.getPayload();
+		IpProtocol protocol = ip.getProtocol();
 
-						rewriteIPforDNS(udp,newIP);
+		if (sourceMac.equals(dnsMacAddress) || destMac.equals(dnsMacAddress)) { // dns
+																				// server
+			if (protocol != IpProtocol.UDP)
+				return Command.CONTINUE;
 
-						// TODO: send PACKET_OUT with modified DNS
-						
+			UDP udp = (UDP) ip.getPayload();
+			if (udp.getSourcePort().getPort() == DNS.DNS_PORT || udp.getDestinationPort().getPort() == DNS.DNS_PORT) {
+				DNS dns = new DNS(udp.getPayload());
+				OFFactory factory = sw.getOFFactory();
+				logger.debug("DNS packet: " + dns.getType());
+				if (dns.getType() == DNS.Type.RESPONSE) {
+					if (dns.getAnswerCount() > 0) {
+						logger.debug("Writing modified response...");
+						DNSResource answer = dns.getAnswers()[0];
+						answer.setIPv4Address(IPv4Address.of("1.2.3.4"));
 					}
-		 
-				} else if (eth.getEtherType() == EthType.ARP) {
-					
-					ARP arp = (ARP) eth.getPayload();
-		 
-					
-					boolean gratuitous = arp.isGratuitous();
-		 
-				} else {
-					// Not sure
+					udp.setPayload(dns);
+					ArrayList<OFAction> actions = new ArrayList<OFAction>();
+					actions.add(factory.actions().buildOutput() // builder pattern used throughout
+							.setPort(OFPort.of(1)) // raw types replaced with objects for type-checking and readability
+							.build()); // list of immutable OFAction objects
+					sw.write(factory.buildPacketOut().setData(ethernet.serialize()).setActions(actions).build());
+				} else if (dns.getType() == DNS.Type.QUERY) {
+					logger.debug("Writing PACKET_MOD to allow incoming queries...");
+					ArrayList<OFAction> actions = new ArrayList<OFAction>();
+					actions.add(factory.actions().buildOutput() // builder pattern used throughout
+							.setPort(OFPort.LOCAL) // raw types replaced with objects for type-checking and readability
+							.build()); // list of immutable OFAction objects
+					OFFlowAdd flow = factory.buildFlowAdd()
+							.setMatch(factory.buildMatch()
+									.setExact(MatchField.IN_PORT, OFPort.of(1))
+									.build()) // immutable Match object
+						.setPriority(100)
+						.setActions(actions)
+						.setBufferId(OFBufferId.NO_BUFFER)
+						.setHardTimeout(10)
+						.build(); // immutable OFFlowMod; no lengths to set; no wildcards to set
+					sw.write(flow);
+
+					sw.write(factory.buildPacketOut().setData(ethernet.serialize()).build());
 				}
-				break;
-			default:
-				break;
+			}
+		} else if (sourceMac.equals(webserverMacAddress) || destMac.equals(webserverMacAddress)) { // web
+																									// server
+			if (protocol != IpProtocol.TCP)
+				return Command.CONTINUE;
+
+			logger.debug("TCP");
+			// if (CapabilitiesManager.getInstance()
+			// .verifyRecord(dstIp.getBytes()) ==
+			// CapabilitiesManager.Action.ALLOW) {
+			// OFFactory factory = sw.getOFFactory();
+			//
+			// ArrayList<OFAction> actions = new ArrayList<OFAction>();
+			// actions.add(factory.actions().buildOutput().setPort(OFPort.LOCAL).build());
+			//
+			// OFFlowAdd flow = factory.buildFlowAdd()
+			// .setMatch(factory.buildMatch().setExact(MatchField.IN_PORT,
+			// OFPort.of(1))
+			// .setExact(MatchField.IPV4_SRC,
+			// srcIp).setExact(MatchField.IPV4_DST, dstIp)
+			// .build())
+			// .setActions(actions).setOutPort(OFPort.of(2)).setBufferId(OFBufferId.NO_BUFFER)
+			// .setHardTimeout(10).build();
+			// sw.write(flow);
+			//
+			// } else {
+			// // TODO: DROP PACKET
+			// System.out.println("Dropping packet!");
+			// }
+
 		}
 		return Command.CONTINUE;
 	}
-
-		
-	private static final int UDP_HEAD_SIZE = 8; // bytes
-	private static final int DNS_OFFSET_DATA = 10; // bytes after name
-	private static final int DNS_OFFSET_TTL = 4; // bytes after name
-
-	private void rewriteIPforDNS(UDP packet, byte[] newIP) {
-		byte[] rawPacket = packet.serialize();
-		int dnsHeaderSize = DNS_OFFSET_DATA + getDNSNameSize(rawPacket); // bytes
-
-		// TODO: we may want to add a verification of the DNS response type to make sure we aren't overwriting packets that don't correspond with an A record
-
-		int totalHead = UDP_HEAD_SIZE + dnsHeaderSize;
-
-		for (int i = 0; i < newIP.length; i++) {
-			rawPacket[totalHead + i] = newIP[i];
-		}
-
-		packet.deserialize(rawPacket,0,packet.getLength());
-	}
-	private int extractTTLfromDNS(UDP packet) {
-		byte[] rawPacket = packet.serialize();
-		int dnsOffset = DNS_OFFSET_TTL + getDNSNameSize(rawPacket); // bytes
-		int offset = UDP_HEAD_SIZE + dnsOffset;
-
-		int result = 0;
-		result += (rawPacket[offset+0] << 24);
-		result += (rawPacket[offset+1] << 16);
-		result += (rawPacket[offset+2] << 8);
-		result += (rawPacket[offset+3]);
-
-		return result;
-	}
-	private byte[] getDNSNameSize(byte[] rawPacket) {
-		int i = UDP_HEAD_SIZE;
-		byte current = rawPacket[i];
-		while (current != 0x00) {
-			current = rawPacket[++i];
-		}
-		return i + 1 - UDP_HEAD_SIZE;
-	}
-
 }
--- src/main/java/cs4516/team4/Capability.java
+++ src/main/java/cs4516/team4/Capability.java
@@ -1,5 +1,6 @@
-
 package cs4516.team4;
+
+import java.util.Date;
 
 public class Capability {
 	public Date creation;
